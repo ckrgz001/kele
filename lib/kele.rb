499756ad6506e5bdd0297e96259b64e5b38d1a82
ckrgz001 <ckrgz001@outlook.com>
2018-02-25 18:38:07 -0500

    Availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -21,6 +21,18 @@ class Kele
     @user = JSON.parse(response.body)
   end
 
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    available = []
+     response.each do |timeslot|
+       if timeslot["booked"] == nil
+         available.push(timeslot)
+       end
+     end
+     puts available
+  end
+
+
   private 
   
     def api_url(endpoint) #method to define web address as the end point
