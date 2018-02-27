d7c1080fed2c59987d92c4f87faf7a3783ec0ea6
ckrgz001 <ckrgz001@outlook.com>
2018-02-26 20:01:09 -0500

    removed hardcode

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,8 +4,6 @@ require 'roadmap'
 
 class Kele
   include HTTParty
-  base_uri "https://www.bloc.io/api/v1/"
-
   #To retrieve the authentication token include HTTParty in Kele,
   #use  self.class.post, and pass in the sessions URL along with username and password.
 
